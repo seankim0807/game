0d51c0ccfeafea713694f00c2b05c92d2602a43a
Sean Kim <skim8705@gmail.com>
2024-08-22 13:51:10 -0700

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 
+function getComputerChoice(){
+    let random_value = Math.floor(Math.random() * 3);
+    let list_game = ["rock", "paper", "scissors"]
+    return list_game[random_value]
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let human_choice = prompt("Pick rock, paper or scissors")
+    return human_choice
+}
+
+console.log(getHumanChoice())
