d5d735a7ba164535529dff65eaf8353737fe6ab8
Sean Kim <skim8705@gmail.com>
2024-08-22 13:54:40 -0700

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 
 function getComputerChoice(){
     let random_value = Math.floor(Math.random() * 3);
-    let list_game = ["rock", "paper", "scissors"]
-    return list_game[random_value]
+    let list_game = ["rock", "paper", "scissors"];
+    return list_game[random_value];
 }
 
 console.log(getComputerChoice())
 
 function getHumanChoice(){
-    let human_choice = prompt("Pick rock, paper or scissors")
+    let human_choice = prompt("Please enter your choice: rock, paper or scissors").toLowerCase
+    while (choice!= "rock" && choice!= "paper" && choice!= "scissors"){
+        choice = prompt("Invalid choice. Please enter rock, paper, or scissors").toLowerCase();
+    }
     return human_choice
 }
 
